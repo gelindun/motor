d86125cb8a603f3a86f38854e546cce93e0b3fc4
叶平 <yeping@yeping.local>
2016-03-25 19:48:10 +0800

    page update

---
+++ App/Home/Controller/PageController.class.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+//s_type about,company,product,help
+class PageController extends HomeController {
+   
+
+    public function _initialize() {
+         parent::_initialize();
+         
+    }
+
+    public function index(){
+        $D_Page = D('site\Page');
+        $this->_arr['keyArr'] = $D_Page->pgKeys();
+        
+        $_action = I('get.act');
+        
+        $_key = I('get.key');
+        if($this->_arr['keyArr'][$_key]){
+            $_where = array('s_key'=>$_key);
+            $_resPage = $D_Page->where($_where)->find();
+            if($_resPage){
+                $this->_arr['resPage'] = $_resPage;
+                $this->_arr['resPage']['content'] = htmlspecialchars_decode($this->_arr['resPage']['content']);
+            }
+        }else{
+            exit;
+        }
+        $this->_arr['pageTitle'] = $this->_arr['keyArr'][$_key]['title'];
+        $this->_arr['seo_title'] = $this->_arr['seo_keywords'] = $this->_arr['pageTitle'];
+        $this->_arr['seo_description'] = msubstr(strip_tags($this->_arr['resPage']['content']),0,100);
+        $this->_arr['s_key'] = $_key;
+        $this->_showDisplay();
+    }
+
+    
+    
+   
+    
+}
