79f28b87411f2ff34140fa8745f68ea8625ec4cb
叶平 <yeping@yeping.local>
2016-03-26 18:18:25 +0800

    cylinder pay form add

---
+++ App/Home/Controller/CylinderController.class.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Home\Controller;
+use Think\Controller;
+//发动机清洗 支付表单
+class CylinderController extends HomeController {
+   
+
+    public function _initialize() {
+         parent::_initialize();
+         
+    }
+
+    public function index(){
+        
+        $this->_showDisplay();
+    }
+
+    
+    
+   
+    
+}
