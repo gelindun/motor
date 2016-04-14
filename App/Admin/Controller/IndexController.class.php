2727de80e0771086dde4a4a18ec75de95326aebb
叶平 <yeping@yeping.local>
2016-04-15 00:10:34 +0800

    device control panel update

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -10,7 +10,27 @@ class IndexController extends AdminController {
     }
     
     public function index(){
-        
+        $D_Device= D('site\Device');
+        $D_Merchant = D('site\Merchant');
+        $_key_word = I('get.keyword');
+        $_where = array(
+            "delete" => array("eq","0")
+        );
+        if($_key_word){
+            $_map["device_name"] = array('EXP','REGEXP \'^.*'.$_key_word.'.*$\'');
+            $_map["device_sn"] = array('EXP','REGEXP \'^.*'.$_key_word.'.*$\'');
+            $_map["remark"] = array('EXP','REGEXP \'^.*'.$_key_word.'.*$\'');
+            $_map['_logic'] = 'or';
+            $_where['_complex'] = $_map;
+        }
+        $_order = array("time_add"=>'DESC');
+        $resList = $D_Device->getPagesize($_where,$this->pgSize,$_order);
+        foreach($resList['lists'] as $k=>$v){
+            $resList['lists'][$k]['store_name'] = $D_Merchant->where(
+                    array('id' => $v['mid'])
+                )->getField('store_name');
+        }
+        $this->_arr['resList'] = $resList;
         $this->_showDisplay();
         
     }
